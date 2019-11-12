b11991dab0fa863f75ed5313fec86c9528192941
Le Tran The Toan <letranthetoan@gmail.com>
2019-11-12 20:07:11 +0700

    make login function at header nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,26 @@ class UsersController < ApplicationController
     
   end
 
+  def sign_up_or_login
+    user = User.find_for_authentication(:email => params[:email])
+    if user&.valid_password?(params[:password])
+      sign_in(user, scope: :user)
+    else
+      user = User.new(email: params[:email], password: params[:password])
+      sign_in(user, scope: :user) if user.save
+    end
+
+    respond_to do |format|
+      format.js 
+    end
+  end
+
   def delete
+    sign_out
     
+    respond_to do |format|
+      format.js 
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_090419) do
+ActiveRecord::Schema.define(version: 2019_11_12_125452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_11_12_090419) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", default: ""
+    t.string "description", default: ""
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
