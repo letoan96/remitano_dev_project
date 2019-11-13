845468b9d40e20e90493796f65d71b2c21ac29a6
Le Tran The Toan <letranthetoan@gmail.com>
2019-11-13 11:31:17 +0700

    refactor and styling

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Video, type: :model do
   before(:all) do
      @user = create(:user)
   end
+  
   context 'Video should be valid' do
     it 'when all video params are provided' do
       video = build(:video, user_id: @user.id)
